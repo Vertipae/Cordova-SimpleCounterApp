d46995e3c43a6b6d24ac9fa95f9043e0da25779a
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-09-27 16:04:13 +0300

    Cool Counter app

---
+++ myVSCordApp1/www/scripts/script.js
@@ -0,0 +1,20 @@
+﻿function modify_qty(val) {
+
+    /* variables */
+    var qty = document.getElementById('qty').value;
+    /* (qty, 10) second parameter is the base of the number, default 10 so you don't really need that */
+    var new_qty = parseInt(qty, 10) + val;
+    /* variable new_qty can't be smaller than zero*/
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+
+    /* get's the qty and put it's value to new_qty and returns it*/
+    document.getElementById('qty').value = new_qty;
+    return new_qty;
+}
+
+function modify_reset() {
+    /* "" = String, '' = Varchar */
+    document.getElementById("qty").value = 0;
+}
